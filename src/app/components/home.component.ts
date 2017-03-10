71e57dc89350252a722bfed7310a91292523c638
DawidvanGraan <dawidvangraan@Dawids-MacBook-Pro-2.local>
2017-03-10 08:51:16 +0200

    Check if I’m the one cooking

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -42,6 +42,8 @@ export class HomeComponent {
           return this.usersServices.getUserForKey(lunch.userId)
             .map(user => {
               lunch.chef = user;
+              // Check if I'm the one cooking
+              lunch.meCooking = (user.uid == this.usersServices.uid);
               return lunch;
           });
         })
